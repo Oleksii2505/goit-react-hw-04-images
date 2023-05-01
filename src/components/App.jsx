5e4d0ec6a33339d1a950dd2f271a92b4eaa46fa3
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-05-01 21:47:52 +0300

    Fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,85 +7,6 @@ import { Modal } from './Modal/Modal';
 import { Loader } from './Loader/Loader';
 import { fetchData } from '../services';
 
-// export class App extends Component {
-//   state = {
-//     searchQuery: '',
-//     hits: [],
-//     page: 1,
-//     isLoading: false,
-//     buttonLoading: false,
-//     showButton: false,
-//     showModal: false,
-//     largeImage: '',
-//     error: '',
-//   }
-
-//   async componentDidUpdate(prevProps, prevState) {
-//     const prevSearchQuery = prevState.searchQuery;
-//     const nextSearchQuery = this.state.searchQuery;
-//     const prevPage = prevState.page;
-//     const nextPage = this.state.page;
-
-//     if (prevSearchQuery !== nextSearchQuery || prevPage !== nextPage) {
-      
-//         this.setState({ isLoading: true });
-//         fetchData(nextSearchQuery, nextPage)
-//         .then(data => {
-          
-//         if(data.hits.length === 0) {
-//           alert('Enter another word to search');
-//           return;
-//         }
-       
-//         this.setState(prev => ({hits: [...prev.hits, ...data.hits],
-//         showButton: this.state.page < Math.ceil(data.totalHits / 12)
-//         }))
-       
-      
-//       }) 
-//       .catch (err => {
-//         console.log(err);
-//         this.setState({ error: err.message });
-//       }) 
-//       .finally (() => {
-//         this.setState({ isLoading: false });
-//     });
-//     }
-//   }    
-
-//   handleSubmit(searchWord) {
-//     this.setState({hits: [] , page: 1 ,searchQuery: searchWord.toLowerCase().trim() });
-//   };
-
-//   onLoadMore = () => {
-
-//     this.setState(prevState =>({ page: prevState.page + 1 }));
-//   };
-
-//   showModal = image => {
-//     this.setState({ largeImage: image, showModal: true });
-//   };
-
-//   closeModal = () => {
-//     this.setState({ largeImage: '', showModal: false });
-//   };
-
-//   render() {
-//     const { hits, isLoading, showButton, showModal, largeImage } = this.state;
-//     return (
-//       <AppComponent>
-//         <SearchBar onSubmit={this.handleSubmit.bind(this)} />
-//         <ImageGallery hits={hits} showModal={this.showModal} />
-//         {isLoading && <Loader />}
-//         {showButton && <LoadMoreBtn onClick={this.onLoadMore} />}
-//         {showModal && (
-//           <Modal closeModal={this.closeModal} largeImage={largeImage} />
-//         )}
-//       </AppComponent>
-//     );
-//   }
-// }
-
 export const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [hits, setHits] = useState([]);
@@ -95,7 +16,6 @@ export const App = () => {
   const [showButton, setShowButton] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [largeImage, setLargeImage] = useState('');
-  const [error, setError] = useState('');
 
   useEffect(() => {
     if (!searchQuery) return;
@@ -113,7 +33,7 @@ export const App = () => {
       })
       .catch(err => {
         console.log(err);
-        setError(err.message);
+        alert('Error');
       })
       .finally(() => {
         setIsLoading(false);
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,49 +5,6 @@ import { ModalOverlay, ModalContentWindow } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-// export class Modal extends Component {
-
-//   static propTypes = {
-//     closeModal: PropTypes.func.isRequired,
-//     largeImage: PropTypes.string.isRequired,
-// };
-  
-//  componentDidMount() {
-//     window.addEventListener("keydown", this.closeModalOnEsc);
-//  }
-
-//  componentWillUnmount() {
-//    window.removeEventListener("keydown", this.closeModalOnEsc);
-//  }
-
-//  closeModalOnEsc = event => {
-//      if (event.code === 'Escape') {
-//          this.props.closeModal();
-//      }
-//  }
-
-//  closeModalOnClickBackdrop = event => {
-//      if (event.target === event.currentTarget) {
-//          this.props.closeModal();
-//      }
-//  }
-
-//  render () {
-//   const { largeImage} = this.props;
-//   return createPortal(
-//         <ModalOverlay
-//         onClick={this.closeModalOnClickBackdrop}
-//         >
-//           <ModalContentWindow>
-//             <img src={largeImage} alt="tag" />
-//           </ModalContentWindow>
-//         </ModalOverlay>,
-//         modalRoot
-//       );
-//  }
-
-// }
-
 export const Modal = ({ closeModal, largeImage }) => {
   const closeModalOnEsc = useCallback(
     (event) => {
